2483cb452b97d7b9bc4b8323d36fefa8f86bde33
nevo1945 <nevo23ti@mahasiswa.pcr.ac.id>
2025-06-22 11:56:02 +0700

    update app

--- src/pages/admin/auth/ProtectedAdminRoute.jsx
+++ src/pages/admin/auth/ProtectedAdminRoute.jsx
@@ -1,6 +1,11 @@
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedAdminRoute({ children }) {
-  const isLoggedIn = localStorage.getItem("adminLoggedIn") === "true";
-  return isLoggedIn ? children : <Navigate to="/admin/auth/not-found" />;
+  const isAuthenticated = localStorage.getItem("adminLoggedIn") === "true";
+
+  if (!isAuthenticated) {
+    return <Navigate to="/admin/auth/not-found" />;
+  }
+
+  return children;
 }
